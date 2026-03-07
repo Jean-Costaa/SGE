ae6560f5be559ba24c98fece274fcc148961eaf0
Jean-Costaa <jeanf1998@gmail.com>
2026-03-07 11:41:13 -0300

    Métricas de produtos no contexto

--- app/views.py
+++ app/views.py
@@ -2,5 +2,14 @@ from django.shortcuts import render
 
 
 def home(request):
-    context = {}
+    product_metrics = {
+        'total_cost_price': 100000,
+        'total_selling_price': 30000,
+        'total_quantity': 1000,
+        'total_profit': 20000,
+    }
+    
+    context = {
+        'product_metrics':product_metrics
+    }
     return render(request, 'home.html', context)
