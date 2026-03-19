7834ad88832b5d727cb5a81ea9cf409079cde0e6
Jean-Costaa <jeanf1998@gmail.com>
2026-03-19 11:10:19 -0300

    Criando API dos domínios

---
+++ suppliers/serializers.py
@@ -0,0 +1,9 @@
+from rest_framework import serializers
+from . import models
+
+
+class SupplierSerializers(serializers.ModelSerializer):
+    
+    class Meta:
+        model = models.Supplier
+        fields = '__all__'
