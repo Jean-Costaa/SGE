09692b6be314a950fbf2a77641f4909ab895ccb9
Jean-Costaa <jeanf1998@gmail.com>
2026-04-11 09:14:34 -0300

    Update notify.py

--- services/notify.py
+++ services/notify.py
@@ -4,11 +4,11 @@ import requests
 class Notify:
 
     def __init__(self):
-        self.__base_url = 'https://webhook.site'
+        self.__base_url = 'http://localhost:8001'
 
     
     def send_event(self, data):
         requests.post(
-            url=f'{self.__base_url}/3bfd437c-658c-40c9-b415-4cb4b23ddbc5',
+            url=f'{self.__base_url}/api/v1/webhooks/order/',
             json=data,
         )
